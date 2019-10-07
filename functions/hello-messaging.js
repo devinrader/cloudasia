351fe9e9dc98230136336ab25e56f849cc77e13b
Devin Rader <devin@twilio.com>
2019-10-07 12:06:43 +0800

    'provision'

--- functions/hello-messaging.js
+++ functions/hello-messaging.js
@@ -1,5 +1,5 @@
 exports.handler = function(context, event, callback) {
   let twiml = new Twilio.twiml.MessagingResponse();
-  twiml.message('Hello World');
+  twiml.message('Hello From Cloud Expo Asia');
   callback(null, twiml);
 };
